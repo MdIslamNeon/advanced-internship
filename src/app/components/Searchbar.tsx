3eebe92636d61e8aab33b1ece5e80c6f828d6a85
MdIslamNeon <2003raisul@gmail.com>
2026-08-13 23:30:16 -0700

    Search bar design complete

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from "./Searchbar.module.css";
+import { IoIosSearch } from "react-icons/io";
+
 
 function Searchbar() {
   return (
-    <div>
-      
+    <div className={styles.search__background}>
+      <div className={styles.search__wrapper}>
+        <div className={styles.search__content}>
+          <div className={styles.search}>
+            <div className={styles.search__input_wrapper}>
+              <input className={styles.search__input} type="text" placeholder="Search for books" />
+              <div className={styles.search__icon}>
+                <IoIosSearch />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,17 +1,26 @@
-import styles from './Sidebar.module.css';
-import Image from 'next/image';
+"use client";
+
+import styles from "./Sidebar.module.css";
+import Image from "next/image";
 import logo from "../../../public/logo.png";
 import { IoHomeOutline } from "react-icons/io5";
 import { CiBookmark } from "react-icons/ci";
 import { TiPencil } from "react-icons/ti";
 import { IoIosSearch } from "react-icons/io";
-
+import { CiSettings } from "react-icons/ci";
+import { HiOutlineQuestionMarkCircle } from "react-icons/hi2";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { useAppDispatch } from "@/redux/hooks";
+import { openModal } from "@/redux/modalSlice";
 
 function Sidebar() {
+
+  const dispatch = useAppDispatch();
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebar__logo}>
-        <Image className={styles.sidebar__img} src={logo} alt='landing logo'/>
+        <Image className={styles.sidebar__img} src={logo} alt="landing logo" />
       </div>
       <div className={styles.sidebar__wrapper}>
         <div className={styles.sidebar__top}>
@@ -27,13 +36,17 @@ function Sidebar() {
             </div>
             <div className={styles.sidebar__link__text}>My Library</div>
           </a>
-          <div className={`${styles.sidebar__link__wrapper} ${styles.sidebar__link__wrapper_link_not_allowed}`}>
+          <div
+            className={`${styles.sidebar__link__wrapper} ${styles.sidebar__link__wrapper_link_not_allowed}`}
+          >
             <div className={styles.sidebar__icon__wrapper}>
               <TiPencil />
             </div>
             <div className={styles.sidebar__link__text}>Highlights</div>
           </div>
-          <div className={`${styles.sidebar__link__wrapper} ${styles.sidebar__link__wrapper_link_not_allowed}`}>
+          <div
+            className={`${styles.sidebar__link__wrapper} ${styles.sidebar__link__wrapper_link_not_allowed}`}
+          >
             <div className={styles.sidebar__icon__wrapper}>
               <IoIosSearch />
             </div>
@@ -41,11 +54,30 @@ function Sidebar() {
           </div>
         </div>
         <div className={styles.sidebar__bottom}>
-
+          <a className={styles.sidebar__link__wrapper} href="/for-you">
+            <div className={styles.sidebar__icon__wrapper}>
+              <CiSettings />
+            </div>
+            <div className={styles.sidebar__link__text}>Settings</div>
+          </a>
+          <div
+            className={`${styles.sidebar__link__wrapper} ${styles.sidebar__link__wrapper_link_not_allowed}`}
+          >
+            <div className={styles.sidebar__icon__wrapper}>
+              <HiOutlineQuestionMarkCircle />
+            </div>
+            <div className={styles.sidebar__link__text}>Search</div>
+          </div>
+          <div className={styles.sidebar__link__wrapper}>
+            <div className={styles.sidebar__icon__wrapper}>
+              <FaArrowRightFromBracket />
+            </div>
+            <div className={styles.sidebar__link__text} onClick={() => dispatch(openModal('login'))}>Login</div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
