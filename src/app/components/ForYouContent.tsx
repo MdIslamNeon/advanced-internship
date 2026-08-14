e9c19252a6f4f59514a8db68dbc77ea3b0b90b54
MdIslamNeon <2003raisul@gmail.com>
2026-08-14 12:44:13 -0700

    For you (selected for you) progressing

--- src/app/components/ForYouContent.tsx
+++ src/app/components/ForYouContent.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import styles from './ForYouContent.module.css';
 
 function ForYouContent() {
   return (
-    <div>
-      
+    <div className={styles.row}>
+      <div className={styles.container}>
+        <div className={styles.for_you_wrapper}>
+          <div className={styles.for_you_title}>Selected just for you</div>
+            <a href="/book" className={styles.selected__book}>
+              <div className={styles.selected__book_sub_title}>
+                  How Constant Innovation Creates Radically Successful Businesses
+              </div>
+              <div className={styles.selected__book_line}></div>
+              <div className={styles.selected__book_content}></div>
+            </a>
+        </div>
+      </div>
     </div>
   )
 }
