971d31fe7b32ea133eb29be449b14c6ef730fe3e
MdIslamNeon <2003raisul@gmail.com>
2026-08-14 15:22:24 -0700

    Started recommended books section

--- src/app/components/ForYouContent.tsx
+++ src/app/components/ForYouContent.tsx
@@ -1,22 +1,65 @@
 import styles from './ForYouContent.module.css';
+import axios from 'axios';
 
-function ForYouContent() {
+type Book = {
+  id: string;
+  author: string;
+  title: string;
+  subTitle: string;
+  imageLink: string;
+  audioLink: string;
+  averageRating: number;
+  type: string;
+};
+
+async function ForYouContent() {
+  const { data } = await axios.get<Book[]>(
+    'https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected'
+  );
+
+  // The endpoint returns an array; the selected book is the first entry.
+  const book = data[0];
+
   return (
-    <div className={styles.row}>
-      <div className={styles.container}>
-        <div className={styles.for_you_wrapper}>
-          <div className={styles.for_you_title}>Selected just for you</div>
-            <a href="/book" className={styles.selected__book}>
-              <div className={styles.selected__book_sub_title}>
-                  How Constant Innovation Creates Radically Successful Businesses
-              </div>
+    <div className={styles.for_you__background}>
+      <div className={styles.row}>
+        <div className={styles.container}>
+          <div className={styles.for_you_wrapper}>
+            <div className={styles.for_you_title}>Selected just for you</div>
+            {/* Selected Book */}
+            <a href={`/book/${book.id}`} className={styles.selected__book}>
+              <div className={styles.selected__book_sub_title}>{book.subTitle}</div>
               <div className={styles.selected__book_line}></div>
-              <div className={styles.selected__book_content}></div>
+              <div className={styles.selected__book_content}>
+                <figure className={styles.book__image_wrapper}>
+                  <img
+                    className={styles.book__image}
+                    src={book.imageLink}
+                    alt={book.title}
+                  />
+                </figure>
+                <div className={styles.selected__book_text}>
+                  <div className={styles.selected__book_title}>{book.title}</div>
+                  <div className={styles.selected__book_author}>{book.author}</div>
+                  <div className={styles.selected__book_duration_wrapper}>
+  
+                  </div>
+                </div>
+              </div>
             </a>
+            {/* Recommended Books */}
+            <div className={styles.recommended__books}>
+                <div className={styles.for_you_title}>Recommended For You</div>
+                <div className={styles.for_you_subtitle}>We think you&apos;ll like these</div>
+                <div className={styles.for_you_recommended_books}>
+
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ForYouContent
+export default ForYouContent;
